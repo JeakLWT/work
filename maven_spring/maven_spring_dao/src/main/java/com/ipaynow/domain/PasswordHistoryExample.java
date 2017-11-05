58cfffee5b95f5580e5b49c8eace353e851bf8b2
jeakLWT <LWT170210@163.com>
2017-11-05 21:37:42 +0800

    2

--- maven_spring/maven_spring_dao/src/main/java/com/ipaynow/domain/PasswordHistoryExample.java
+++ maven_spring/maven_spring_dao/src/main/java/com/ipaynow/domain/PasswordHistoryExample.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 
 public class PasswordHistoryExample {
+    private String name;
     protected String orderByClause;
 
     protected boolean distinct;
